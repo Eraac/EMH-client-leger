92d07ae3677b1c2c45cd33a0fac14199d9452ff5
Kévin Labesse <k.labesse@aforp.eu>
2015-04-19 21:08:17 +0200

    Suppression d'un espace en trop

--- src/HIA/FormBundle/Entity/Register.php
+++ src/HIA/FormBundle/Entity/Register.php
@@ -30,7 +30,7 @@ class Register
 
     /**
      * @ORM\ManyToOne(targetEntity="HIA\FormBundle\Entity\Field")
-     * @ORM\JoinColumn(name="field_id", referencedColumnName="id",  onDelete="SET NULL")
+     * @ORM\JoinColumn(name="field_id", referencedColumnName="id", onDelete="SET NULL")
      */
     private $field;
 
